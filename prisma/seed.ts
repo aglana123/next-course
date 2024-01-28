5a316d2a540927070a7d3e29b2290a5113cd3b9b
BagasKelana <hilalbagas@gmail.com>
2024-01-28 17:38:58 +0700

    feat: seed for category course

---
+++ prisma/seed.ts
@@ -0,0 +1,43 @@
+const { PrismaClient } = require('@prisma/client');
+const db = new PrismaClient();
+
+async function main() {
+	try {
+		const categoriesData = [
+			{
+				name: 'Web Development',
+				slug: 'web-development',
+			},
+			{
+				name: 'Mobile Development',
+				slug: 'mobile-development',
+			},
+			{
+				name: 'Data Science',
+				slug: 'data-science',
+			},
+			{
+				name: 'Machine Learning',
+				slug: 'machine-learning',
+			},
+		];
+
+		const createCategoryPromises = categoriesData.map(
+			async (categoryData) => {
+				return await db.category.create({
+					data: categoryData,
+				});
+			}
+		);
+
+		const [web, mobile, data_science, machine_learning] = await Promise.all(
+			createCategoryPromises
+		);
+
+		console.log({ web, mobile, data_science, machine_learning });
+	} catch (error) {
+		console.error('Error:', error);
+	}
+}
+
+main();
