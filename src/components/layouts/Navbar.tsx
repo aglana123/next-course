9315526643f10a7439a8b636bf8f8102d545c822
BagasKelana <hilalbagas@gmail.com>
2024-01-30 13:47:20 +0700

    feat: navbar

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,35 +1,25 @@
-import {
-	BellIcon,
-	MenuIcon,
-	MessageCircle,
-	MessageSquare,
-	SearchIcon,
-	ShoppingCart,
-} from 'lucide-react';
 import SearchBar from '@/components/layouts/SearchBar';
-import UserAccountNav from '@/components/layouts/UserMenuNav';
-import CategoriesPopUp from '@/components/layouts/CategoriesPopUp';
 import MainLogo from '@/components/layouts/MainLogo';
-import { Button } from '../ui/button';
-import { Separator } from '../ui/separator';
-import NavMenu from './NavMenu';
+import NavMenu from '@/components/layouts/NavMenu';
+import HumburgerMenu from '@/components/layouts/HumburgerMenu';
+import CategoriesPopUp from './CategoriesPopUp';
 
 const Navbar = () => {
 	return (
 		<>
-			<header className="w-full h-[64px] lg:h-[80px] shadow-md shadow-black/5 bg-white">
+			<header className="w-full h-[64px] lg:h-[80px] shadow-md shadow-black/5 bg-white z-[50] fixed top-0">
 				<nav className="container px-4 md:px-8 xl:px-16 h-full flex gap-4 items-center justify-between lg:justify-start">
 					{/* left */}
-					<Button
-						variant="ghost"
-						className="lg:hidden h-fit w-fit p-2">
-						<MenuIcon />
-					</Button>
+					<HumburgerMenu />
 					<MainLogo />
-					{/* center */}
-					<SearchBar />
-					{/* right */}
 
+					{/* center */}
+					<div className="w-full hidden lg:flex gap-4 xl:mx-4">
+						<CategoriesPopUp />
+						<SearchBar />
+					</div>
+
+					{/* right */}
 					<NavMenu />
 				</nav>
 			</header>
