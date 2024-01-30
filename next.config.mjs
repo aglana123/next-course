f8fed5e5187399f54000f841494ae69d0f900442
BagasKelana <hilalbagas@gmail.com>
2024-01-30 12:49:21 +0700

    fix(config): next js image url

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'utfs.io',
+			},
+			{
+				protocol: 'https',
+				hostname: 'lh3.googleusercontent.com',
+				pathname: '**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'uploadthing.com',
+				pathname: '**',
+			},
+		],
+	},
+};
 
 export default nextConfig;
