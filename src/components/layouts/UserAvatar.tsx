5f032b613a5c597cae8ff961dc0ee4b5ebd3dc12
BagasKelana <hilalbagas@gmail.com>
2024-01-30 13:48:56 +0700

    feat: user avatar navbar

---
+++ src/components/layouts/UserAvatar.tsx
@@ -0,0 +1,34 @@
+import { User } from 'next-auth';
+import { FC } from 'react';
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
+import Image from 'next/image';
+
+import { AvatarProps } from '@radix-ui/react-avatar';
+
+interface UserAvatarProps extends AvatarProps {
+	user: Pick<User, 'image' | 'name'>;
+	sizeImg: number;
+    
+}
+
+const UserAvatar: FC<UserAvatarProps> = ({ user, sizeImg, ...props }) => {
+	return (
+		<Avatar {...props}>
+			{user.image ? (
+				<Image
+					src={user.image}
+					alt="Profile picture"
+					referrerPolicy="no-referrer"
+					width={sizeImg}
+					height={sizeImg}
+				/>
+			) : (
+				<AvatarFallback>
+					<span className="sr-only">{user?.name}</span>
+				</AvatarFallback>
+			)}
+		</Avatar>
+	);
+};
+
+export default UserAvatar;
