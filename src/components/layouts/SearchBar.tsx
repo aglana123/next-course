2559c3c1cc3078ae69b4d00bef99e1f7fb5667b0
BagasKelana <hilalbagas@gmail.com>
2024-01-30 13:47:13 +0700

    feat(navbar): search bar

--- src/components/layouts/SearchBar.tsx
+++ src/components/layouts/SearchBar.tsx
@@ -1,19 +1,38 @@
+'use client';
+
 import { Input } from '@/components/ui/input';
 import { SearchIcon } from 'lucide-react';
-import CategoriesPopUp from './CategoriesPopUp';
-import { Button } from '../ui/button';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 const SearchBar = () => {
+	const [term, setTerm] = useState('');
+	const router = useRouter();
+
+	const handleOnKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === 'Enter') {
+			e.preventDefault();
+			if (e.currentTarget.value) {
+				const url = `/courses?q=${term}`;
+				router.push(url);
+			}
+		}
+	};
+
+	const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setTerm(() => e.target.value);
+	};
 	return (
-		<div className="w-full hidden lg:flex gap-4 xl:mx-4">
-			<CategoriesPopUp />
-			<div className="flex w-full items-center ring-1 ring-input bg-slate-50  hover:ring-primary focus-within:ring-primary pl-2 pr-4 rounded-3xl select-none">
-				<Input
-					placeholder="Search courses here..."
-					className="border-none bg-transparent"
-				/>
-				<SearchIcon className="text-black/40 h-6 w-6 cursor-pointer" />
-			</div>
+		<div className="flex w-full items-center ring-1 ring-input bg-slate-50  hover:ring-primary focus-within:ring-primary pl-2 pr-4 rounded-3xl select-none">
+			<Input
+				onKeyDown={handleOnKeyDown}
+				onChange={handleOnChange}
+				value={term}
+				placeholder="Cari kursus disini..."
+				className="border-none bg-transparent"
+			/>
+			<SearchIcon className="text-black/40 h-6 w-6 cursor-pointer" />
 		</div>
 	);
 };
