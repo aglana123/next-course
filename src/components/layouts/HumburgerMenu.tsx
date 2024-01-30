8abe32b63e50889c71e4001d76f2227204659e4d
BagasKelana <hilalbagas@gmail.com>
2024-01-30 13:46:33 +0700

    setup

---
+++ src/components/layouts/HumburgerMenu.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { MenuIcon } from 'lucide-react';
+import { Button } from '../ui/button';
+
+const HumburgerMenu = () => {
+	return (
+		<Button
+			variant="ghost"
+			className="lg:hidden h-fit w-fit p-2">
+			<MenuIcon />
+		</Button>
+	);
+};
+
+export default HumburgerMenu;
