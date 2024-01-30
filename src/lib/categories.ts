913d3063e553dadea3f4b6f040bfebc45686a0c1
BagasKelana <hilalbagas@gmail.com>
2024-01-30 13:48:44 +0700

    feat: categories db

---
+++ src/lib/categories.ts
@@ -0,0 +1,11 @@
+import { Category } from '@prisma/client';
+import db from './db';
+
+export const fetchCategories = async () => {
+	const categories = await db.category.findMany();
+
+	if (!categories?.length) {
+		return null;
+	}
+	return categories as Category[];
+};
