828ccbe96db646332ba6fc9bde863f34cd5804f4
BagasKelana <hilalbagas@gmail.com>
2024-01-30 13:48:12 +0700

    feat(type): course-pageProps

---
+++ src/types/course-page.d.ts
@@ -0,0 +1,10 @@
+export type CoursePageParams = {
+	searchParams: {
+		limit?: string;
+		page?: string;
+		q?: string;
+		level?: string;
+		category_id?: string;
+		public_access?: string;
+	};
+};
