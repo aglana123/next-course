c0f129e1dad3d771bf214ce285e7bcda5c65c7db
BagasKelana <hilalbagas@gmail.com>
2024-01-30 13:43:23 +0700

    setup

---
+++ src/app/(lobby)/(routes)/courses/loading.tsx
@@ -0,0 +1,5 @@
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+
+export default function ProductsLoading() {
+	return <div className="w-full min-h-screen bg-red-400"></div>;
+}
---
+++ src/app/(lobby)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<main className="">
+			<div>INI HOME Page</div>
+		</main>
+	);
+}
