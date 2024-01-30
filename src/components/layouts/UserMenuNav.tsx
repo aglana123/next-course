f624aa90be59f0866fd4db29fbf9e9513f5fbb1a
BagasKelana <hilalbagas@gmail.com>
2024-01-30 13:49:07 +0700

    feat: user avatar menu

--- src/components/layouts/UserMenuNav.tsx
+++ src/components/layouts/UserMenuNav.tsx
@@ -6,31 +6,40 @@ import {
 	PopoverTrigger,
 } from '@/components/ui/popover';
 import Link from 'next/link';
-import { Avatar } from '@/components/ui/avatar';
-import Image from 'next/image';
 import { Separator } from '@/components/ui/separator';
+import { signOut } from 'next-auth/react';
 import { Book, BookMarked, LogOutIcon, ShoppingBag } from 'lucide-react';
+import { User } from 'next-auth';
+import { UserId } from '@/types/next-auth';
+import { UserRole } from '@prisma/client';
+import UserAvatar from './UserAvatar';
 
 const userMenu = [
-	{ title: 'My Learning', icon: Book },
-	{ title: 'My Teaching', icon: BookMarked },
-	{ title: 'My Cart', icon: ShoppingBag },
+	{ title: 'My Learning', icon: Book, role: 'STUDENT' },
+	{ title: 'My Teaching', icon: BookMarked, role: 'TEACHER' },
+	{ title: 'My Cart', icon: ShoppingBag, role: 'STUDENT' },
 ];
 
-const UserMenuNav = () => {
+type UserMenuNavProps = {
+	currentUser: User & {
+		id: UserId;
+		role: UserRole;
+	};
+};
+
+const UserMenuNav: React.FC<UserMenuNavProps> = ({ currentUser }) => {
+	const { name, image, email, role } = currentUser;
 	return (
 		<Popover>
 			<PopoverTrigger>
-				<Avatar>
-					<Image
-						width={100}
-						height={100}
-						alt="user navbar avatar"
-						src={'/profile_nana.jpg'}
-					/>
-				</Avatar>
+				<UserAvatar
+					user={{
+						name: name || null,
+						image: image || null,
+					}}
+					sizeImg={100}
+				/>
 			</PopoverTrigger>
-
 			<PopoverContent
 				align="end"
 				avoidCollisions={true}
@@ -39,39 +48,54 @@ const UserMenuNav = () => {
 				<div className="flex flex-col w-full">
 					<Link href={''}>
 						<div className="flex gap-4 px-4">
-							<Avatar className="h-16 w-16">
-								<Image
-									width={200}
-									height={200}
-									alt="user navbar avatar"
-									src={'/profile_nana.jpg'}
-								/>
-							</Avatar>
+							<UserAvatar
+								className="h-16 w-16"
+								user={{
+									name: name || null,
+									image: image || null,
+								}}
+								sizeImg={200}
+							/>
 
 							<div className="w-full self-center">
-								<h3>Bagas Kelana</h3>
-								<small>bagaslana@gmail.com</small>
+								<h3>{name}</h3>
+								<small>{email}</small>
 							</div>
 						</div>
 					</Link>
+					<Separator className="mt-4 mb-2" />
+					{userMenu.map((menu) =>
+						role === 'TEACHER' ? (
+							<Link
+								key={menu.title}
+								href="/teacher-courses">
+								<div className="flex items-center gap-2 py-2 px-4 hover:text-primary ">
+									{<menu.icon className="h-4 w-4" />}
+									{menu.title}
+								</div>
+							</Link>
+						) : (
+							menu.role !== 'TEACHER' && (
+								<Link
+									key={menu.title}
+									href="">
+									<div className="flex items-center gap-2 py-2 px-4 hover:text-primary ">
+										{<menu.icon className="h-4 w-4" />}
+										{menu.title}
+									</div>
+								</Link>
+							)
+						)
+					)}
 					<Separator className="my-2" />
-					{userMenu.map((menu) => (
-						<Link
-							key={menu.title}
-							href="">
-							<div className="flex items-center gap-2 py-2 px-4 hover:text-primary ">
-								{<menu.icon className="h-4 w-4" />}
-								{menu.title}
-							</div>
-						</Link>
-					))}
-					<Separator className="my-2" />
-					<Link href="">
-						<div className="flex gap-2 items-center px-4 py-2 text-destructive">
-							<LogOutIcon className="h-4 w-4" />
-							Logout
-						</div>
-					</Link>
+					<div
+						onClick={() => {
+							signOut();
+						}}
+						className="flex gap-2 items-center px-4 py-2 text-destructive cursor-pointer">
+						<LogOutIcon className="h-4 w-4" />
+						Logout
+					</div>
 				</div>
 			</PopoverContent>
 		</Popover>
