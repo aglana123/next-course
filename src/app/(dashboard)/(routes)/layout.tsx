0a1c36b0ac67797f83655c5363908cfc634d7ce8
BagasKelana <hilalbagas@gmail.com>
2024-01-30 13:41:35 +0700

    feat: layout dashboard & private route teacher

--- src/app/(dashboard)/(routes)/layout.tsx
+++ src/app/(dashboard)/(routes)/layout.tsx
@@ -1,8 +1,17 @@
-const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+import { redirect } from 'next/navigation';
+import Sidebar from '../_components/sidebar';
+import { getAuthSession } from '@/lib/auth';
+
+const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
+	const session = await getAuthSession();
+	const user = session?.user;
+	if (user?.role !== 'TEACHER') {
+		return redirect('/');
+	}
 	return (
-		<div>
-			<h1> ini dashboard layout</h1>
-			<div>{children}</div>
+		<div className="w-full h-full flex flex-1">
+			<Sidebar />
+			<div className="ml-[300px] container py-6">{children}</div>
 		</div>
 	);
 };
