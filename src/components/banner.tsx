e0a96b85b945bf12be7e7db4eba81624ccecd511
BagasKelana <hilalbagas@gmail.com>
2024-01-31 13:38:58 +0700

    feat: benner component

---
+++ src/components/banner.tsx
@@ -0,0 +1,41 @@
+import { AlertTriangle, CheckCircleIcon } from 'lucide-react';
+import { cva, type VariantProps } from 'class-variance-authority';
+
+import { cn } from '@/lib/utils';
+
+const bannerVariants = cva(
+	'border text-center p-4 text-sm flex items-center w-full',
+	{
+		variants: {
+			variant: {
+				warning:
+					'bg-yellow-200/80 text-primary shadow-sm shadow-black/10',
+				success:
+					'bg-emerald-700 text-secondary shadow-sm shadow-black/10',
+			},
+		},
+		defaultVariants: {
+			variant: 'warning',
+		},
+	}
+);
+
+interface BannerProps extends VariantProps<typeof bannerVariants> {
+	label: string;
+}
+
+const iconMap = {
+	warning: AlertTriangle,
+	success: CheckCircleIcon,
+};
+
+export const Banner = ({ label, variant }: BannerProps) => {
+	const Icon = iconMap[variant || 'warning'];
+
+	return (
+		<div className={cn(bannerVariants({ variant }))}>
+			<Icon className="h-4 w-4 mr-2 " />
+			{label}
+		</div>
+	);
+};
