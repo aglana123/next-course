44c92e6e1781c49a4ab04f7c96f4e4108fa22501
BagasKelana <hilalbagas@gmail.com>
2024-02-01 22:11:02 +0700

    feat: layout user/lobby pages

--- src/app/(lobby)/layout.tsx
+++ src/app/(lobby)/layout.tsx
@@ -1,13 +1,15 @@
 import Navbar from '@/components/layouts/Navbar';
+import { getAuthSession } from '@/lib/auth';
 
 type LobbyLayoutProps = {
 	children: React.ReactNode;
 };
 
 const LobbyLayout: React.FC<LobbyLayoutProps> = async ({ children }) => {
+	const currentUser = await getAuthSession();
 	return (
 		<>
-			<Navbar />
+			<Navbar user={currentUser?.user} />
 			<div>{children}</div>
 		</>
 	);
