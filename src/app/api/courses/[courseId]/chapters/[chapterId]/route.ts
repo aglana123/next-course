cc83212e2585125aa5ca0d204aed29672c76b469
BagasKelana <hilalbagas@gmail.com>
2024-02-02 15:48:30 +0700

    feat(api): update & delete chapter

---
+++ src/app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -0,0 +1,81 @@
+import { NextResponse } from 'next/server';
+
+import db from '@/lib/db';
+import { getAuthSession } from '@/lib/auth';
+
+export async function PATCH(
+	req: Request,
+	{ params }: { params: { chapterId: string; courseId: string } }
+) {
+	try {
+		const session = await getAuthSession();
+		const { chapterId, courseId } = params;
+		const values = await req.json();
+
+		if (!session?.user) {
+			return new NextResponse('Unauthorized', { status: 401 });
+		}
+
+		const teacher = await db.user.findUnique({
+			where: {
+				id: session.user.id,
+				role: 'TEACHER',
+			},
+		});
+
+		if (!teacher) {
+			return new NextResponse('Forbidden', { status: 403 });
+		}
+
+		const chapter = await db.chapter.update({
+			where: {
+				id: chapterId,
+				courseId,
+			},
+			data: {
+				...values,
+			},
+		});
+		return NextResponse.json(chapter);
+	} catch (error) {
+		console.log('[CHAPTER_ID]', error);
+		return new NextResponse('Internal Error', { status: 500 });
+	}
+}
+
+export async function DELETE(
+	req: Request,
+	{ params }: { params: { chapterId: string; courseId: string } }
+) {
+	try {
+		const session = await getAuthSession();
+		const { chapterId, courseId } = params;
+
+		if (!session?.user) {
+			return new NextResponse('Unauthorized', { status: 401 });
+		}
+
+		const teacher = await db.user.findUnique({
+			where: {
+				id: session.user.id,
+				role: 'TEACHER',
+			},
+		});
+
+		if (!teacher) {
+			return new NextResponse('Forbidden', { status: 403 });
+		}
+
+		const chapter = await db.chapter.delete({
+			where: {
+				id: chapterId,
+				courseId,
+				Course: { author_id: teacher.id },
+			},
+		});
+		return NextResponse.json(chapter);
+	} catch (error) {
+		console.log('[CHAPTER_ID]', error);
+		return new NextResponse('Internal Error', { status: 500 });
+	}
+}
