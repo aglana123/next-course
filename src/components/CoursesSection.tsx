6f86ed14855167e071b9e88aa01d86c32c4b28b2
BagasKelana <hilalbagas@gmail.com>
2024-02-03 17:36:48 +0700

    fix(api): slug, course_access search courses

--- src/components/CoursesSection.tsx
+++ src/components/CoursesSection.tsx
@@ -1,19 +1,25 @@
 'use client';
 
-import { coursesAsset } from '@/asset/courses-asset';
 import CourseCard from '@/components/cards/CourseCard';
+import { Course } from '@prisma/client';
+import { FC } from 'react';
 
-const CoursesSection = () => {
+type CoursesSectionProps = {
+	courses: (Course & { author: { name: string } })[];
+};
+
+const CoursesSection: FC<CoursesSectionProps> = ({ courses }) => {
 	return (
 		<section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-8 place-items-stretch">
-			{coursesAsset.map((course) => (
+			{courses.map((course) => (
 				<CourseCard
+					slug={course.slug}
 					key={course.id}
 					title={course.title}
-					desc={course.description}
-					price={+course.price}
-					src={course.image}
-					teacher={course.teacher}
+					desc={course.description!}
+					src={course.imageUrl!}
+					teacher={course.author.name}
+					course_access={course.public_access}
 				/>
 			))}
 		</section>
