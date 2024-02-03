1b18aa4cc49c9fe2b6a1b48e008bc91cdf199aea
BagasKelana <hilalbagas@gmail.com>
2024-02-03 17:42:04 +0700

    fix: add course_access & slug

--- src/components/cards/CourseCard.tsx
+++ src/components/cards/CourseCard.tsx
@@ -1,51 +1,67 @@
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
-import { formatRupiah } from '@/helpers/formatRupiah';
+import { PublicAccess } from '@prisma/client';
 import Image from 'next/image';
+import { Lock, BookOpen } from 'lucide-react';
+import Link from 'next/link';
 
 type CourseCardProps = {
 	title: string;
 	desc: string;
-	price: number;
+	course_access: PublicAccess;
 	src: string;
 	teacher: string;
+	slug: string;
 };
 
 const CourseCard: React.FC<CourseCardProps> = ({
 	title,
 	desc,
-	price,
 	src,
 	teacher,
+	course_access,
+	slug,
 }) => {
 	return (
-		<Card className="rounded overflow-hidden">
-			<CardHeader className="p-0">
-				<div className="aspect-square relative">
-					<Image
-						className="object-cover aspect-square"
-						fill
-						quality={80}
-						sizes="(max-width: 400px) 100vw, (max-width: 700px) 50vw, (max-width: 900px) 33vw, 20vw"
-						src={src}
-						alt="card-img"
-						priority
-					/>
-				</div>
-			</CardHeader>
-			<hr />
-			<CardContent className="flex flex-col py-4 px-6 h-full">
-				<h3 className="line-claps-with-ellipsis font-semibold">
-					{title}
-				</h3>
-				<div className="flex flex-col mb-2">
-					<p className="line-claps-with-ellipsis ">{desc}</p>
-					<small className="text-black/80 text-xs">
-						By {teacher}
-					</small>
-				</div>
-				<h3 className="font-semibold">Rp {formatRupiah(price)}</h3>
-			</CardContent>
-		</Card>
+		<Link href={`/course/${slug}`}>
+			<Card className="rounded-md overflow-hidden h-full">
+				<CardHeader className="p-0">
+					<div className="aspect-video relative">
+						<Image
+							className="object-cover aspect-video"
+							fill
+							quality={80}
+							sizes="(max-width: 400px) 100vw, (max-width: 700px) 50vw, (max-width: 900px) 33vw, 20vw"
+							src={src}
+							alt="card-img"
+							priority
+						/>
+					</div>
+				</CardHeader>
+				<hr />
+				<CardContent className="flex flex-col py-4 px-6 h-full">
+					<h3 className="line-claps-with-ellipsis font-semibold">
+						{title}
+					</h3>
+					<div className="flex flex-col mb-2">
+						<p className="line-claps-with-ellipsis ">{desc}</p>
+						<small className="text-black/80 text-xs">
+							By {teacher}
+						</small>
+					</div>
+					{course_access === 'Public' ? (
+						<small className="font-medium flex gap-2 whitespace-nowrap items-center">
+							<BookOpen className="bg-teal-500 rounded-md text-white p-1 h-5 w-5" />{' '}
+							{course_access}
+						</small>
+					) : (
+						<small className="font-medium flex gap-2 whitespace-nowrap items-center">
+							<Lock className="bg-rose-500 rounded-md text-white p-1 h-5 w-5" />{' '}
+							{course_access}
+						</small>
+					)}
+				</CardContent>
+			</Card>
+		</Link>
 	);
 };
 
