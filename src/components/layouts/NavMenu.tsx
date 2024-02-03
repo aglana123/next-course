67d114b6ff50da5767f9b4b09c75d6370f932ee5
BagasKelana <hilalbagas@gmail.com>
2024-02-03 17:44:17 +0700

    fix: nav menu depent on user role

--- src/components/layouts/NavMenu.tsx
+++ src/components/layouts/NavMenu.tsx
@@ -1,50 +1,47 @@
 import { BellIcon, SearchIcon } from 'lucide-react';
-import { Button } from '../ui/button';
-import UserMenuNav from './UserMenuNav';
+
 import NavMessages from './NavMessages';
 import Link from 'next/link';
-import { getAuthSession } from '@/lib/auth';
+import { Button } from '../ui/button';
+import { User } from 'next-auth';
+import UserMenuNav from './UserMenuNav';
 
-const NavMenu = async () => {
-	const currentUser = await getAuthSession();
+const NavMenu = ({ user }: { user?: User }) => {
 	return (
-		<>
-			<div className="flex items-center gap-4">
-				<Button
-					variant="outline"
-					className="lg:hidden h-fit w-fit p-2">
-					<SearchIcon />
-				</Button>
-
-				{currentUser?.user ? (
-					<>
-						<div className="hidden lg:flex gap-4">
-							<Link href={'/courses'}>
-								<span className="whitespace-nowrap text-primary font-medium">
-									My Courses
-								</span>
-							</Link>
-							<BellIcon className="fill-slate-500 stroke-none" />
-							<NavMessages />
-						</div>
-						<UserMenuNav currentUser={currentUser?.user} />
-					</>
-				) : (
-					<div className="flex gap-2">
-						<Link
-							aria-label="Masuk"
-							href={'/sign-in'}>
-							<Button>Masuk</Button>
-						</Link>
-						<Link
-							aria-label="Daftar"
-							href={'/sign-up'}>
-							<Button variant={'outline'}>Daftar</Button>
-						</Link>
-					</div>
-				)}
-			</div>
-		</>
+		<div className="flex items-center gap-4">
+			<Button
+				variant="outline"
+				className="lg:hidden h-fit w-fit p-2">
+				<SearchIcon />
+			</Button>
+			{user ? (
+				<>
+					<Link
+						className="max-lg:hidden"
+						href={'/courses'}>
+						<span className="whitespace-nowrap text-primary font-medium">
+							My Courses
+						</span>
+					</Link>
+					<BellIcon className="fill-slate-500 stroke-none max-lg:hidden" />
+					<NavMessages />
+					<UserMenuNav currentUser={user} />
+				</>
+			) : (
+				<div className="flex items-center gap-2">
+					<Link
+						aria-label="Masuk"
+						href={'/sign-in'}>
+						<Button>Masuk</Button>
+					</Link>
+					<Link
+						aria-label="Daftar"
+						href={'/sign-up'}>
+						<Button variant={'outline'}>Daftar</Button>
+					</Link>
+				</div>
+			)}
+		</div>
 	);
 };
 
