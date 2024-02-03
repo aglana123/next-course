6104861a02b82616e9c39ab45d4e3730da119deb
BagasKelana <hilalbagas@gmail.com>
2024-02-03 17:45:19 +0700

    fix: type

--- src/helpers/firstLetterToUpper.ts
+++ src/helpers/firstLetterToUpper.ts
@@ -1,11 +1,8 @@
-export function firstLetterToUpper(character?: string) {
-    if (character) {
-        const firstLetter = character
-            .split('')
-            .map((value, index) => (index === 0 ? value.toUpperCase() : value))
-            .join('');
+export function firstLetterToUpper(character: string) {
+	const firstLetter = character
+		.split('')
+		.map((value, index) => (index === 0 ? value.toUpperCase() : value))
+		.join('');
 
-        return firstLetter;
-    }
-    return '';
+	return firstLetter;
 }
