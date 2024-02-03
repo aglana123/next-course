50a7062267ed2ff826f3cedf174318b7140e250f
BagasKelana <hilalbagas@gmail.com>
2024-02-03 16:53:51 +0700

    feat(api): unpublish chapter

---
+++ src/app/api/courses/[courseId]/chapters/[chapterId]/unpublish/route.ts
@@ -0,0 +1,43 @@
+import { NextResponse } from 'next/server';
+
+import db from '@/lib/db';
+import { getAuthSession } from '@/lib/auth';
+
+export async function PATCH(
+	req: Request,
+	{ params }: { params: { chapterId: string; courseId: string } }
+) {
+	try {
+		const session = await getAuthSession();
+		const { chapterId, courseId } = params;
+
+		if (!session?.user) {
+			return new NextResponse('Unauthorized', { status: 401 });
+		}
+
+		const teacher = await db.user.findUnique({
+			where: {
+				id: session.user.id,
+				role: 'TEACHER',
+			},
+		});
+
+		if (!teacher) {
+			return new NextResponse('Forbidden', { status: 403 });
+		}
+
+		const chapter = await db.chapter.update({
+			where: {
+				id: chapterId,
+				courseId,
+			},
+			data: {
+				is_published: false,
+			},
+		});
+		return NextResponse.json(chapter);
+	} catch (error) {
+		console.log('[UNPUBLISH]', error);
+		return new NextResponse('Internal Error', { status: 500 });
+	}
+}
