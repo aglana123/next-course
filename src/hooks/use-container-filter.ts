23c2d8e0adf64e6ee5400318fd45fd1bdf52d11e
BagasKelana <hilalbagas@gmail.com>
2024-02-04 23:41:47 +0700

    feat: hook for container courses

--- src/hooks/use-container-filter.ts
+++ src/hooks/use-container-filter.ts
@@ -2,12 +2,10 @@ import { create } from 'zustand';
 
 type ConfettiStore = {
 	isOpen: boolean;
-	onOpen: () => void;
-	onClose: () => void;
+	onToggle: () => void;
 };
 
 export const useContainerFilter = create<ConfettiStore>((set) => ({
 	isOpen: false,
-	onOpen: () => set({ isOpen: true }),
-	onClose: () => set({ isOpen: false }),
+	onToggle: () => set((state) => ({ isOpen: !state.isOpen })),
 }));
