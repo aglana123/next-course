0035ade63a1f62769ca8d79fe9a06da8c7cf6338
BagasKelana <hilalbagas@gmail.com>
2024-02-05 04:48:28 +0700

    fix: become teacher button, update session, update db

--- src/app/(lobby)/_components/home-page-action.tsx
+++ src/app/(lobby)/_components/home-page-action.tsx
@@ -1,14 +1,56 @@
-import { Button } from '@/components/ui/button';
+'use client';
 
-const HomePageAction = () => {
+import { Button } from '@/components/ui/button';
+import { $Enums } from '@prisma/client';
+import { User } from 'next-auth';
+import { useSession } from 'next-auth/react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { FC, useEffect } from 'react';
+import toast from 'react-hot-toast';
+
+type HomePageActionProps = {
+	user:
+		| (User & {
+				id: string;
+				role: $Enums.UserRole;
+		  })
+		| null;
+};
+
+const HomePageAction: FC<HomePageActionProps> = ({ user }) => {
+	const { update, status } = useSession();
+	const route = useRouter();
+
+	const handleUpladeSession = async () => {
+		try {
+			if (!user) {
+				return route.push('/sign-in');
+			}
+			if (user.role === 'TEACHER') {
+				return route.push('/teacher/courses');
+			}
+			await update({ role: 'TEACHER' });
+			toast.success('you become teacher now');
+			return route.push('/teacher/courses');
+		} catch (error) {
+			console.log(error);
+			toast.error('error occure');
+		}
+	};
+
 	return (
 		<div className="flex gap-4 max-lg:justify-center">
+			<Link href="/courses">
+				<Button
+					variant="secondary"
+					className="rounded-md px-5 py-3 text-base font-medium h-fit">
+					Enroll Courses
+				</Button>
+			</Link>
 			<Button
-				variant="secondary"
+				onClick={handleUpladeSession}
 				className="rounded-md px-5 py-3 text-base font-medium h-fit">
-				Enroll Courses
-			</Button>
-			<Button className="rounded-md px-5 py-3 text-base font-medium h-fit">
 				Become Teacher
 			</Button>
 		</div>
