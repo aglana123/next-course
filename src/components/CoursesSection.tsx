c2dbf082dfc11a03df69f7e2a1c9adee38b1bdac
BagasKelana <hilalbagas@gmail.com>
2024-02-05 00:08:03 +0700

    style : layouting

--- src/components/CoursesSection.tsx
+++ src/components/CoursesSection.tsx
@@ -10,7 +10,7 @@ type CoursesSectionProps = {
 
 const CoursesSection: FC<CoursesSectionProps> = ({ courses }) => {
 	return (
-		<section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-8 place-items-stretch">
+		<section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6 place-items-stretch">
 			{courses.map((course) => (
 				<CourseCard
 					slug={course.slug}
