7ad2fa7927db48670966db1af561c12965194278
BagasKelana <hilalbagas@gmail.com>
2024-02-06 02:57:12 +0700

    feat:messages

--- src/components/layouts/NavMessages.tsx
+++ src/components/layouts/NavMessages.tsx
@@ -1,13 +1,15 @@
 'use client';
 
+import useWishlist from '@/hooks/use-wishlist';
 import { MessageCircle } from 'lucide-react';
 
 const NavMessages = () => {
+	const { items } = useWishlist();
 	return (
 		<div className="relative">
 			<MessageCircle className="fill-primary stroke-none" />
 			<div className="absolute h-4 w-4 flex items-center justify-center top-0 -right-1 rounded-full bg-destructive text-white text-[10px] p-2 font-medium">
-				2
+				{items?.length || 0}
 			</div>
 		</div>
 	);
