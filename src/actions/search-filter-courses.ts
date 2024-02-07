88ec02ce24b3b5c46e14cd432554b24fed77d95e
BagasKelana <hilalbagas@gmail.com>
2024-02-07 07:38:17 +0700

    feat(server-action) : fetching filter courses

---
+++ src/actions/search-filter-courses.ts
@@ -0,0 +1,64 @@
+import { firstLetterToUpper } from '@/helpers/firstLetterToUpper';
+import db from '@/lib/db';
+import { Course, CoursesLevel, PublicAccess } from '@prisma/client';
+
+type QueryProps = {
+  title?: { contains: string; mode: 'insensitive' };
+  is_published: boolean;
+  category_id?: string;
+  level?: CoursesLevel;
+  public_access?: PublicAccess;
+};
+
+type SearchParamsProps = {
+  limit?: string;
+  page?: string;
+  q?: string;
+  level?: CoursesLevel;
+  category?: string;
+  access?: PublicAccess;
+  sort?: string;
+};
+
+type CoursesReturn = (Course & { author: { name: string } })[];
+
+const searchFilterCourses = async (searchParams: SearchParamsProps) => {
+  const { q: slug, category, level, access, sort } = searchParams;
+  const query: QueryProps = { is_published: true };
+  const sorting: { createdAt?: 'desc' | 'asc' } = {};
+
+  if (slug) {
+    query.title = { contains: slug, mode: 'insensitive' };
+  }
+
+  if (category) {
+    query.category_id = category;
+  }
+
+  if (level) {
+    query.level = firstLetterToUpper(level) as CoursesLevel;
+  }
+
+  if (access) {
+    query.public_access = firstLetterToUpper(access) as PublicAccess;
+  }
+
+  if (sort && sort !== 'relevance') {
+    sorting.createdAt = 'desc';
+  }
+
+  try {
+    const courses = await db.course.findMany({
+      where: query,
+      include: { author: { select: { name: true } } },
+      orderBy: sorting
+    });
+
+    return courses as CoursesReturn;
+  } catch (error) {
+    console.error('Error fetching courses:', error);
+    throw error;
+  }
+};
+
+export default searchFilterCourses;
