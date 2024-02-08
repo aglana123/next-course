dfbb61acd7be1bc72577dce90a6a2d5b62f5e071
BagasKelana <hilalbagas@gmail.com>
2024-02-09 04:20:43 +0700

    feat: get progress user chapters

--- src/app/(lobby)/(routes)/course/[courseSlug]/chapters/layout.tsx
+++ src/app/(lobby)/(routes)/course/[courseSlug]/chapters/layout.tsx
@@ -2,6 +2,7 @@ import { redirect } from 'next/navigation';
 import db from '@/lib/db';
 import { getAuthSession } from '@/lib/auth';
 import { CourseSidebar } from './_components/course-sidebar';
+import { getProgress } from '@/actions/get-progress';
 
 const CourseLayout = async ({
   children,
@@ -18,12 +19,25 @@ const CourseLayout = async ({
 
   const course = await db.course.findUnique({
     where: {
-      slug: params.courseSlug
+      is_published: true,
+      slug: params.courseSlug,
+      enrolled_courses: {
+        some: {
+          userId: session.user.id
+        }
+      }
     },
     include: {
       chapters: {
         where: {
           is_published: true
+        },
+        include: {
+          userProgress: {
+            where: {
+              userId: session.user.id
+            }
+          }
         },
         orderBy: {
           position: 'asc'
@@ -36,10 +50,12 @@ const CourseLayout = async ({
     return redirect('/');
   }
 
+  const progress = await getProgress(session.user.id, course.id);
+
   return (
     <div className="h-full">
       <div className="hidden lg:flex h-full w-80  pt-[64px] lg:pt-[80px] flex-col fixed inset-y-0 right-0 bg-white shadow shadow-black/20">
-        <CourseSidebar progressCount={80} course={course} />
+        <CourseSidebar progressCount={progress} course={course} />
       </div>
       <main className="lg:pr-80 pt-[64px] lg:pt-[80px] h-full">{children}</main>
     </div>
