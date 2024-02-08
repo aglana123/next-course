9118d9d6c67cf82cc821c8c91ee4ff7b7fff96a9
BagasKelana <hilalbagas@gmail.com>
2024-02-09 04:23:07 +0700

    fix: route for my-courses/my-learning

---
+++ src/app/(lobby)/(routes)/user/learning/page.tsx
@@ -0,0 +1,24 @@
+import { getCourses } from '@/actions/get-courses';
+
+import { getAuthSession } from '@/lib/auth';
+
+import { redirect } from 'next/navigation';
+import { CoursesList } from './_components/courses-list';
+
+const UserCoursesPage = async () => {
+  const session = await getAuthSession();
+
+  if (!session?.user) {
+    return redirect('/');
+  }
+
+  const userCourses = await getCourses({ userId: session.user.id });
+
+  return (
+    <div className="container px-0 md:px-4 xl:px-16 py-4 mt-[64px] lg:mt-[80px] w-full">
+      <CoursesList items={userCourses} />
+    </div>
+  );
+};
+
+export default UserCoursesPage;
