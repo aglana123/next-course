5a2601900196e3ed74b3e213a4052a4a59c418dc
BagasKelana <hilalbagas@gmail.com>
2024-02-09 04:17:16 +0700

    feat & fix: enroll course for auth user

--- src/app/(lobby)/(routes)/course/[courseSlug]/_components/action-section.tsx
+++ src/app/(lobby)/(routes)/course/[courseSlug]/_components/action-section.tsx
@@ -1,44 +1,11 @@
 'use client';
-
 import { Button } from '@/components/ui/button';
 import useWishlist from '@/hooks/use-wishlist';
-import { Course, PublicAccess } from '@prisma/client';
-import axios from 'axios';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import toast from 'react-hot-toast';
+import { Course } from '@prisma/client';
+import Link from 'next/link';
 
-const ActionSection = ({
-  courseId,
-  publicAccess,
-  course
-}: {
-  courseId: string;
-  publicAccess: PublicAccess;
-  course: Course;
-}) => {
-  const { data: session } = useSession();
+const ActionSection = ({ course }: { course: Course }) => {
   const { addItem } = useWishlist();
-  const [isLoading, setIsLoading] = useState(false);
-  const router = useRouter();
-  const handleEnrollCourse = async () => {
-    try {
-      if (!session?.user) {
-        return router.push('/sign-in');
-      }
-      setIsLoading(true);
-      const enrollCourse = await axios.post(`/api/courses/${courseId}/enroll`);
-      toast.success('Enroll Success');
-      console.log(enrollCourse);
-      router.push('/my-courses');
-    } catch (error) {
-      console.log(error);
-      toast.error('Something went wrong');
-    } finally {
-      setIsLoading(false);
-    }
-  };
 
   const AddToWishlist = () => {
     addItem(course);
@@ -46,29 +13,15 @@ const ActionSection = ({
   return (
     <div className=" py-4 w-full flex flex-col gap-4 justify-center ">
       <div className="flex flex-col gap-4 justify-center">
-        {publicAccess === 'Public' ? (
+        <Link href={'/sign-in'}>
           <Button
-            isLoading={isLoading}
-            onClick={handleEnrollCourse}
             variant={'ghost'}
-            className="h-fit font-medium py-4 text-lg rounded-md bg-primary text-primary-foreground hover:text-primary-foreground hover:bg-primary/90"
+            className="h-fit w-full font-medium py-4 text-lg rounded-md bg-primary text-primary-foreground hover:text-primary-foreground hover:bg-primary/90"
           >
             Enroll Now
           </Button>
-        ) : (
-          <>
-            <Button
-              isLoading={isLoading}
-              onClick={handleEnrollCourse}
-              variant={'ghost'}
-              className="h-fit font-medium py-4 text-lg rounded-md bg-primary text-primary-foreground hover:text-primary-foreground hover:bg-primary/90"
-            >
-              Get Access
-            </Button>
-          </>
-        )}
+        </Link>
         <Button
-          isLoading={isLoading}
           onClick={AddToWishlist}
           variant={'secondary'}
           className="h-fit py-4 text-lg px-6 font-medium rounded-md"
@@ -76,13 +29,6 @@ const ActionSection = ({
           Add to Wishlist
         </Button>
       </div>
-      {publicAccess === 'Private' && (
-        <span className="text-xs text-destructive">
-          *Kursus ini memiliki akses privat. Klik{' '}
-          <strong className="text-xs">{`"Get Access" `}</strong>
-          untuk meminta izin pemilik kursus.
-        </span>
-      )}
     </div>
   );
 };
---
+++ src/app/(lobby)/(routes)/course/[courseSlug]/_components/auth-action-section.tsx
@@ -0,0 +1,106 @@
+'use client';
+
+import GetAccessModal from '@/components/modals/GetAccessModal';
+import { Button } from '@/components/ui/button';
+import useWishlist from '@/hooks/use-wishlist';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import toast from 'react-hot-toast';
+import { CourseDataType } from './desktop-course-section';
+
+const AuthActionSection = ({
+  isPublicAccess,
+  course,
+  hasAccessToCourse,
+  isEnrolledInCourse
+}: {
+  isPublicAccess: boolean;
+  course: CourseDataType;
+  hasAccessToCourse: boolean;
+  isEnrolledInCourse: boolean;
+}) => {
+  const { addItem } = useWishlist();
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const { id: courseId } = course;
+
+  const handleEnrollCourse = async () => {
+    if (isEnrolledInCourse) {
+      return router.push(
+        `/course/${course.slug}/chapters/${course.chapters?.[0]?.id}`
+      );
+    }
+    try {
+      setIsLoading(true);
+      const enrollCourse = await axios.post(`/api/courses/${courseId}/enroll`);
+      toast.success('Enroll Success');
+      console.log(enrollCourse);
+      router.push('/my-courses');
+    } catch (error) {
+      console.log(error);
+      toast.error('Something went wrong');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleGetAccess = async () => {
+    try {
+      setIsLoading(true);
+      const privateAccessResponse = await axios.post(
+        `/api/courses/${courseId}/enroll/access/user`
+      );
+      toast.success(
+        'Success. Please await permission from the owner to access this course.'
+      );
+      console.log(privateAccessResponse);
+    } catch (error) {
+      console.log(error);
+      toast.error('Something went wrong');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const AddToWishlist = () => {
+    addItem(course);
+  };
+  return (
+    <div className=" py-4 w-full flex flex-col gap-4 justify-center ">
+      <div className="flex flex-col gap-4 justify-center">
+        {isPublicAccess || hasAccessToCourse || isEnrolledInCourse ? (
+          <Button
+            isLoading={isLoading}
+            onClick={handleEnrollCourse}
+            variant={'ghost'}
+            className="h-fit font-medium py-4 text-lg rounded-md bg-primary text-primary-foreground hover:text-primary-foreground hover:bg-primary/90"
+          >
+            {isEnrolledInCourse ? 'Go to Course' : 'Enroll Now'}
+          </Button>
+        ) : (
+          <GetAccessModal onConfirm={handleGetAccess}>
+            <Button
+              isLoading={isLoading}
+              variant={'ghost'}
+              className="h-fit font-medium py-4 text-lg rounded-md bg-primary text-primary-foreground hover:text-primary-foreground hover:bg-primary/90"
+            >
+              Get Access
+            </Button>
+          </GetAccessModal>
+        )}
+        <Button
+          isLoading={isLoading}
+          onClick={AddToWishlist}
+          variant={'secondary'}
+          className="h-fit py-4 text-lg px-6 font-medium rounded-md"
+        >
+          Add to Wishlist
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default AuthActionSection;
