2febab58874a35495c4beae7e68b0519923a2235
BagasKelana <hilalbagas@gmail.com>
2024-02-10 02:28:15 +0700

    fix(style): svg stroke style

--- src/components/layouts/wishlist-menu.tsx
+++ src/components/layouts/wishlist-menu.tsx
@@ -10,9 +10,9 @@ const WishlistMenu = () => {
   const hasItems = Boolean(items.length);
   return (
     <div className="relative max-lg:hidden">
-      <Heart className="fill-rose-500  stroke-rose-500" />
+      <Heart className="stroke-rose-500" />
       {hasItems && (
-        <div className="absolute h-5 w-5 flex items-center justify-center -top-1 -right-2 rounded-full bg-primary text-white text-xs p-2 font-medium">
+        <div className="absolute h-5 w-5 flex items-center justify-center -top-1 -right-3 rounded-full bg-primary text-white text-xs p-2 font-medium">
           {items.length}
         </div>
       )}
