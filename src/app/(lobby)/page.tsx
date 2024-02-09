c0a0b022d46fd7d14d09cd58cb589f6e6ccb7c59
BagasKelana <hilalbagas@gmail.com>
2024-02-10 05:43:33 +0700

    fix: static rendering

--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -1,7 +1,7 @@
 import FeaturedCategoriesSection from './_components/featured-categories-section';
 import FeaturedCoursesSection from './_components/featured-courses-section';
 import HeroHome from './_components/hero-home';
-export default async function Home() {
+export default function Home() {
   return (
     <main className="mt-[64px] lg:mt-[80px] w-full flex flex-col gap-y-20">
       <section className="relative bg-white/80 max-lg:pb-6">
