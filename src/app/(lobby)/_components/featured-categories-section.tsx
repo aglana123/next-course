bfa2567687f932b939d13001b5b0b349b9ba8393
BagasKelana <hilalbagas@gmail.com>
2024-02-11 08:41:18 +0700

    refactor(style)

--- src/app/(lobby)/_components/featured-categories-section.tsx
+++ src/app/(lobby)/_components/featured-categories-section.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 const FeaturedCategoriesSection = () => {
   return (
     <div className="flex flex-col items-center max-w-screen-xl px-4 mx-auto ">
-      <div className="mb-8 max-w-sm text-center">
+      <div className="mb-8 max-w-md text-center">
         <h2 className="font-bold text-3xl md:text-4xl text-primary mb-2">
           Categories Section
         </h2>
@@ -15,7 +15,7 @@ const FeaturedCategoriesSection = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 place-items-stretch">
         {categoriesAsset.map((category) => (
           <Link key={category.slug} href={`/courses?category${category.slug}`}>
-            <div className="group/category flex flex-col items-center py-8 px-6 gap-4 bg-white rounded-md shadow-md shadow-black/30 ">
+            <div className="group/category flex flex-col items-center py-10 px-6 gap-4  bg-white rounded-md shadow-md shadow-black/30 h-full w-full">
               <Image
                 className="animate-up-down"
                 src={category.img}
@@ -27,7 +27,7 @@ const FeaturedCategoriesSection = () => {
                 <h3 className="font-semibold group-hover/category:text-primary">
                   {category.name}
                 </h3>
-                <p className="max-w-sm">{category.description}</p>
+                <p className="max-w-md">{category.description}</p>
               </div>
             </div>
           </Link>
